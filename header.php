5d96a2ba7079eb099add0f4262322d11881244dd
Boutaina ELYAZIJI <boutaina.mumu11@gmail.com>
2020-03-29 15:59:23 +0100

    Update header.php

--- header.php
+++ header.php
@@ -14,34 +14,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
-    <style>
-      body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
-      body, html 
-        {
-        height: 100%;
-        line-height: 1.8;
-         }
-        /* Full height image header */
-        .bgimg-1 
-        {
-          background-position: center;
-          background-size: cover;
-       /*   background-image:url('/wordpress/wp-content/themes/estsb/img/hello.jpg');
-          */min-height: 100%;
-        }
-        .w3-bar .w3-button 
-        {
-         padding:17px;
-        
-        }
-        .slideanim {visibility:hidden;}
-        .container-fluid 
-        {
-             padding: 60px 50px;
-        }
-
-
-    </style>
 <body >
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
@@ -57,18 +29,18 @@
           </div>
       </div>
       <div class="w3-dropdown-hover w3-hide-small">
-        <button class="w3-button" title="Notifications"> Filères<i class="fa fa-caret-down"></i></button>     
+        <button class="w3-button" title="Notifications"> Filières<i class="fa fa-caret-down"></i></button>     
           <div class="w3-dropdown-content w3-card-4 w3-bar-block">
               <a href="#" class="w3-bar-item w3-button">GI</a>
               <a href="#" class="w3-bar-item w3-button">TM</a>
               <a href="#" class="w3-bar-item w3-button">GA</a>
           </div>
       </div>
-      <a href="#team" class="w3-bar-item w3-button"><i class="fa fa-user"></i> Formation</a>
-      <a href="#work" class="w3-bar-item w3-button"><i class="fa fa-th"></i> Réglement</a>
-      <a href="http://192.168.1.102/wordpress/home/article/" class="w3-bar-item w3-button"><i class="fa fa-user"></i>Article</a>
-      <a href="#pricing" class="w3-bar-item w3-button"><i class="fa fa-user"></i> Suvi des lauréats</a>
-      <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
+      <a href="#team" class="w3-bar-item w3-button"><i class="fa fa-th"></i> Formation</a>
+      <a href="#work" class="w3-bar-item w3-button"><i class="fa fa-group"></i> Réglement</a>
+      <a href="http://192.168.1.102/wordpress/home/article/" class="w3-bar-item w3-button"><i class=" fa fa-calendar-check-o"></i>Articles</a>
+      <a href="#pricing" class="w3-bar-item w3-button"><i class="fa fa-calendar-check-o"></i> Suivi des lauréats</a>
+      <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-address-book"></i> CONTACT</a>
       <div class="container w3-bar-item">
         
         <?php get_search_form()?>
