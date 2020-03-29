a72e6d6eed341b80e10d9999150177ac78ba906e
Boutaina ELYAZIJI <boutaina.mumu11@gmail.com>
2020-03-29 16:25:45 +0100

    Update header.php

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/interfaceWP/wp-content/themes/ESTSBWP/style.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
 <body >
